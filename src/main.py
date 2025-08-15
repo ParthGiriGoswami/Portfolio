5ad625ff60f58e8529a64ff6c7cbcffca75c913c
ParthGiriGoswami <143078824+ParthGiriGoswami@users.noreply.github.com>
2025-08-15 06:04:19 +0530

    Added skills section and formatted education section

--- src/main.py
+++ src/main.py
@@ -18,22 +18,29 @@ async def main(page: ft.Page):
             content=ft.Container(content=ft.Column([
                     ft.Text("B.Tech in Computer Science", weight=ft.FontWeight.BOLD, size=20),
                     ft.Text("Graphic Era Hill University, Bhimtal"),
-                    ft.Text("CGPA: 7.68", color=ft.Colors.BLUE_GREY_700),
+                    ft.Text("CGPA: 7.67", color=ft.Colors.BLUE_700),
                     ft.Text("Graduated: 2025", italic=True, size=12, color=ft.Colors.GREY)
                 ], spacing=5), padding=11)),
         ft.Card(content=ft.Container(content=ft.Column([
-                    ft.Text("12th Grade (Science)", weight=ft.FontWeight.BOLD, size=20),
+                    ft.Text("Senior Secondary", weight=ft.FontWeight.BOLD, size=20),
                     ft.Text("Army Public School, Almora"),
-                    ft.Text("Percentage: 75.8%", color=ft.Colors.BLUE_GREY_700),
+                    ft.Text("Percentage: 74.4%", color=ft.Colors.BLUE_700),
                     ft.Text("Year: 2021", italic=True, size=12, color=ft.Colors.GREY)
                 ], spacing=5), padding=11)),
         ft.Card(content=ft.Container(content=ft.Column([
-                    ft.Text("10th Grade", weight=ft.FontWeight.BOLD, size=20),
+                    ft.Text("High School", weight=ft.FontWeight.BOLD, size=20),
                     ft.Text("Army Public School, Almora"),
-                    ft.Text("Percentage: 85.4%", color=ft.Colors.BLUE_GREY_700),
+                    ft.Text("Percentage: 85.2%", color=ft.Colors.BLUE_700),
                     ft.Text("Year: 2019", italic=True, size=12, color=ft.Colors.GREY)
                 ], spacing=5),padding=11)),
     ], spacing=10)
+    ski = ft.Text("Skills", size=second_text_size, weight=ft.FontWeight.W_600, text_align="center")
+    skilist=ft.Column([
+        ft.ListTile(title=ft.Text("• Python")),
+        ft.ListTile(title=ft.Text("• Java")),
+        ft.ListTile(title=ft.Text("• MySQL")),
+        ft.ListTile(title=ft.Text("• NoSQL")),
+    ])
     pro = ft.Text("Projects", size=second_text_size, weight=ft.FontWeight.W_600, text_align="center")
     project_cards = ft.ResponsiveRow([
         ft.Card(
@@ -74,7 +81,7 @@ async def main(page: ft.Page):
             ft.Row([intro], alignment=ft.MainAxisAlignment.CENTER),
             ft.Row([txt], alignment=ft.MainAxisAlignment.CENTER),
             ft.Divider(), ft.Row([edu], alignment=ft.MainAxisAlignment.CENTER),
-            education_cards, ft.Divider(),
+            education_cards, ft.Divider(), ski, skilist, ft.Divider(),
             ft.Row([pro], alignment=ft.MainAxisAlignment.CENTER), project_cards
         ], horizontal_alignment="center"),
         padding=ft.padding.only(top=30),
